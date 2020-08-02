12aa0c40b80cef0f9546c1c7625b19e0870dc31a
lipskydan <lipsky@knu.ua>
2020-08-02 18:15:45 +0300

    add bd sqlite

--- App/main.py
+++ App/main.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# БД - Таблица - Записи
+# Таблица:
+# id    title    price      isActive
+# 1     Some      100         True
+# 2     Some2     200         False
+# 3     Some3     300         True
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Colum(db.Text, nullable=False)
 
 
 @app.route('/')
@@ -11,6 +30,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    return render_template('create.html')
 
 
 if __name__ == "__main__":
